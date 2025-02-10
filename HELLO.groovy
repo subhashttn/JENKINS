3a188ff286538f9e1d0f281d800d3c261b5320de
subhashttn <subhash.shah@tothenew.com>
2025-02-10 15:39:39 +0530

    Create HELLO.groovy
    new changes

---
+++ HELLO.groovy
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('My name') {
+            steps {
+                
+                echo "WELCOME $name $lastname"
+                
+            }
+        }
+    }
+}
