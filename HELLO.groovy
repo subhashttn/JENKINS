924ef47beb69ca55374bd6b52dc88caca7b1a583
subhashttn <subhash.shah@tothenew.com>
2025-02-11 01:04:56 +0530

    Update HELLO.groovy
    ek or naya commit

--- HELLO.groovy
+++ HELLO.groovy
@@ -6,8 +6,7 @@ pipeline {
             steps {
                 
                 echo "WELCOME $name $lastname"
-                
-            }
+                echo "new commit"            }
         }
     }
 }
